e9127d3e19a0669864d51d5b931bc3795be3aef1
SebasDezar01 <142622833+SebasDezar01@users.noreply.github.com>
2023-08-25 13:59:25 -0500

    Add files via upload

---
+++ src/laboratorio2/Laboratorio2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio2;
+
+/**
+ *
+ * @author ESTUDIANTE
+ */
+public class Laboratorio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora Calc = new Calculadora ();
+        Calc.setLocationRelativeTo(null);//Permite centrar el formulario;
+        Calc.setVisible(true);
+    }
+    
+}
